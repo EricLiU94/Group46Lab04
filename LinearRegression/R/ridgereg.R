4c6d1627e40f6c91f52355c9b76998f05688e68b
Julia Lindohf <julia@julias-imac.mynet>
2019-10-17 21:14:18 +0200

    updates

---
+++ LinearRegression/R/ridgereg.R
@@ -0,0 +1,19 @@
+library(ggplot2)
+#' Ridge Regression Package
+ridgereg <- setRefClass("ridgereg",
+                        fields = list ( 
+                          ),
+                        methods =list (
+                          initialize<- function(formula, data, lambda) {
+                            X <- model.matrix(formula, data)
+                            var_name <<- colnames(X)
+                            
+                            # extarcting the independent variable
+                            t1 <- all.vars(formula)
+                            
+                            # to change the class of the data from a data frame to a list
+                            f1 <- which(names(data) == t1[1])
+                            y1 <- data[f1]
+                            Y <- as.matrix(y1)
+                          }
+                        )
