1739a7c7219ea2d933b83e844f999dcb83e53207
Julia Lindohf <julia@julias-imac.mynet>
2019-10-23 16:48:45 +0200

    updates

--- LinearRegression/R/QR_regression.R
+++ LinearRegression/R/QR_regression.R
@@ -1,11 +1,18 @@
 #' QR Decomposition Calculation 
 QR_decomp <- setRefClass( "QR_Ridge", 
              fields = list (
+               var_name= "character" 
                
              ), 
              methods =list(
                initialize = function (formula, data) {
-                 X <- model.matrix(formula, data)
+                 X <- model.matrix(formula, data) 
+                 var_name <<- colnames(X)
+                 # decomposition of matrix X
+                 D<- qr(X)
+                 UpQ <- qr.Q(D)
+                 LoQ<- qr.R(D)
+                 
                }
              )
   )
